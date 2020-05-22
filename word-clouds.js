70e583d47d9da00557bab46f44024348f45d09dd
Pulipuli Chen <blog@pulipuli.info>
2020-05-22 17:28:23 +0800

    0522 close #2 可以搭配WordClouds了

---
+++ word-clouds.js
@@ -0,0 +1,21 @@
+document.getElementById('download-word-clouds-data').addEventListener('click', function () {
+  let t = d3.select("#text").property("value")
+  
+  var tags = {};
+  var e = {},
+    n = d3.select("#per-line").property("checked");
+  t.split(n ? /\n/g : wordSeparators).forEach(function (t) {
+    discard.test(t) || (n || (t = t.replace(punctuation, "")), stopWords.test(t.toLowerCase()) || ((t = t.substr(0, maxLength)), (e[t.toLowerCase()] = t), (tags[(t = t.toLowerCase())] = (tags[t] || 0) + 1)));
+  }),
+    (tags = d3.entries(tags).sort(function (t, e) {
+      return e.value - t.value;
+    })),
+    tags.forEach(function (t) {
+      t.key = e[t.key];
+    });
+  
+  let output = tags.map(({key, value}) => key + '\t' + value).join('\n')
+  
+  var blob = new Blob([output], {type: "text/plain;charset=utf-8"});
+  saveAs(blob, 'word-cloud_' + (new Date()).mmddhhmm() + '.txt')
+})
